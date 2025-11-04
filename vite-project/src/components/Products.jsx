a59d3b514f57c942fd13a5d1a19ffad9665afb60
Ualine055 <ualine055@gmail.com>
2025-11-04 12:06:52 +0200

    updated content on my product.jsx

--- vite-project/src/components/Products.jsx
+++ vite-project/src/components/Products.jsx
@@ -1,72 +1,12 @@
 import { useState } from 'react';
 import { useCart } from "../context/CartContext";
+import { products, categories } from "../data/products";
 
 
 
 export default function Products() {
   const [selectedCategory, setSelectedCategory] = useState('All');
   const { addToCart } = useCart();
-
-  const categories = ['All', 'Electronics', 'Fashion', 'Accessories'];
-
-  const products = [
-    {
-      id: 1,
-      name: 'Wireless Headphones',
-      category: 'Electronics',
-      price: '25,000 rwf',
-      image: 'https://images.unsplash.com/photo-1546435770-a3e426bf472b?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1165',
-    },
-    {
-      id: 2,
-      name: 'Leather Wallet',
-      category: 'Accessories',
-      price: '49,000 rwf',
-      image: 'https://images.unsplash.com/photo-1630071635402-0f7bf751f646?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687',
-    },
-    {
-      id: 3,
-      name: 'Smartwatch',
-      category: 'Electronics',
-      price: '199,000 rwf',
-      image: 'https://images.unsplash.com/photo-1508685096489-7aacd43bd3b1?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=627',
-    },
-    {
-      id: 4,
-      name: 'Silk Scarf',
-      category: 'Fashion',
-      price: '29,000 rwf',
-      image: 'https://images.unsplash.com/photo-1606259458027-54d2a728b6ab?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735',
-    },
-    {
-      id: 5,
-      name: 'Bluetooth Speaker',
-      category: 'Electronics',
-      price: '79,000 rwf',
-      image: 'https://images.unsplash.com/photo-1589256469067-ea99122bbdc4?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1074',
-    },
-    {
-      id: 6,
-      name: 'Sunglasses',
-      category: 'Accessories',
-      price: '59,000 rwf',
-      image: 'https://images.unsplash.com/photo-1508296695146-257a814070b4?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=880',
-    },
-    {
-      id: 7,
-      name: 'Canvas Tote Bag',
-      category: 'Fashion',
-      price: '39,000 rwf',
-      image: 'https://images.unsplash.com/photo-1721111260419-03565e518110?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687',
-    },
-    {
-      id: 8,
-      name: 'Fitness Tracker',
-      category: 'Electronics',
-      price: '129,000 rwf',
-      image: 'https://images.unsplash.com/photo-1576243345690-4e4b79b63288?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170',
-    },
-  ];
 
   const filteredProducts =
     selectedCategory === 'All'
